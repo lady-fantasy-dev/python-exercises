3790ef2ee17c03fc19d80a43943b60c2ea12134c
lady-fantasy-dev <ykordjaz@gmail.com>
2026-08-21 11:16:41 +0200

    add exercise on conditions

---
+++ Basiswissen_Python/Week_2/10_Tag/purchase_ticket_condition.py
@@ -0,0 +1,20 @@
+'''
+User nach dem Alter fragen,
+und nach seinem Geld
+  
+Wenn der Benutzer erwachsen ist (weil FSK 18),
+und mind. 20 Euro Guthaben hat,
+Artikel (Kinoticket) gekauft.
+'''
+
+def purchase_ticket():
+    age = int(input("How old are you? Enter a number: "))
+    balance = int(input("How much is your account balance? Enter a number: "))
+    
+    if age >= 18 and balance >= 20:
+        print("You can purchase a ticket.")
+    else:
+        print("Sorry, you are too young or don't have sufficient credit")
+
+purchase_ticket()
+    
