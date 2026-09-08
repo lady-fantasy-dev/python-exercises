cd607fe50d9995c6e629aa7a8baa42f690d76146
lady-fantasy-dev <ykordjaz@gmail.com>
2026-09-08 13:52:53 +0200

    dictionary digit to word easy solutiob

---
+++ Python_Programming_PCEP_PCAP/dict_digit_to_word.py
@@ -0,0 +1,28 @@
+# Write a program that propmts the user to enter a phone number
+# and returns the numbers as words.
+
+user_input = input("Please enter your phone number: ")
+
+numbers_dict = {
+    "0": "zero",
+    "1": "one",
+    "2": "two",
+    "3": "three",
+    "4": "four",
+    "5": "five",
+    "6": "six",
+    "7": "seven",
+    "8": "eight",
+    "9": "nine"
+}
+
+def phone_number_to_words(user_input):
+    for i in user_input:
+            try:
+                word = numbers_dict[i]
+                print(word, end = " ")
+
+            except KeyError:
+                 raise Exception("That was not a valid number!")
+
+phone_number_to_words(user_input)
