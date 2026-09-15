79779c3c1c75db7b2ead396a0df1fb95143bd6c1
lady-fantasy-dev <ykordjaz@gmail.com>
2026-09-15 16:26:57 +0200

    add basic error handling

--- Python_Programming_PCEP_PCAP/user_currency_converter.py
+++ Python_Programming_PCEP_PCAP/user_currency_converter.py
@@ -6,16 +6,21 @@ from datetime import date
 
 c = CurrencyConverter()
 
-user_input = float(input("Enter the amount to be converted: "))
-source_currency = input("Choose a source currency: 'USD', 'EUR', or 'CAD': ")
-target_currency = input("Choose a target currency: 'USD', 'EUR', or 'CAD': ")
+while True:
+    try:
+        user_input = float(input("Enter the amount to be converted: "))
+        source_currency = input("Choose a source currency: 'USD', 'EUR', or 'CAD': ")
+        target_currency = input("Choose a target currency: 'USD', 'EUR', or 'CAD': ")
 
-conversion = c.convert(
-    user_input,
-    source_currency,
-    target_currency,
-    date=date(2026, 9, 14)
-    )
+        conversion = c.convert(
+            user_input,
+            source_currency,
+            target_currency,
+            date=date(2026, 9, 14)
+            )
+
+    except ValueError:
+        print("Please enter a valid answer...")
 
 # Test the currency conversion
 print(round(conversion, 2))
